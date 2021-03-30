80162511aa235b77581f65031502689b51e2d278
goldfish0105 <09160341@me.mcu.edu.tw>
2021-03-31 00:22:19 +0800

    add week04 by hand

---
+++ week03/week03-4.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlio.h>
+int a[10];
+int main(){
+    int b[10];
+    int *p=(int*)malloc(sizeof(int)*10);
+    return 0;
+}
--- week05-1.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char line[10]="decline";
-    char line2[10]={'p','r','o','p','e','r','\0'};
-    printf("%s\n",line);
-    printf("%s\n",line2);
-}
