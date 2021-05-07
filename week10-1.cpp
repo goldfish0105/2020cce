cbfc27fe03028b0cd8cf78bf897e4c23c5eaada5
goldfish0105 <79676936+goldfish0105@users.noreply.github.com>
2021-05-07 10:52:50 +0800

    Add files via upload

---
+++ week10-1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+char line[1000];
+int main()
+{
+	int T;
+	scanf("%d\n\n",&T);
+	for(int t=0;t<T;t++){
+		int N;
+		while(gets(line)!=NULL){
+			if(strcmp(line,"")==0)break;
+			printf("%s\n",line);
+		}
+		printf("====¤À¹j½u====\n");
+	}
+}
+
